1ded95cb8ca182428b0ca46f7b6495545fe2fc22
Cute Rich Boy <218043178+chubbyNOTfatRichcuteboy@users.noreply.github.com>
2025-08-24 23:26:56 -0700

    finish 6-11  exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const converted = (f-32)*5/9;
+  return parseFloat(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const converted = (c*9/5)+32;
+  return parseFloat(converted.toFixed(1));
 };
-
+// npm test tempConversion.spec.js
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
